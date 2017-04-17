4f89e77a8e0214a667480db2886b033c24abfccc
ahxxm <i@ahxxm.com>
2017-04-17 20:56:43 +0800

    add missing seq.el for flycheck-settings
    do not change global clang language standard
    enable flycheck for c-mode

--- settings/flycheck-settings.el
+++ settings/flycheck-settings.el
@@ -1,14 +1,4 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2017-02-21 16:05:07 Tuesday by ahxxm>
-
-;; Copyright (C) 2016 Richard Wong
-
-;; Author: Richard Wong
-;; Email: github@cccc.im
-
-;; Version: 0.1
-;; PUBLIC LICENSE: GPLv3
-
 ;;; code:
 (add-to-list 'load-path (concat plugins-path-r "flycheck/"))
 (add-to-list 'load-path (concat plugins-path-r "seq.el"))
@@ -18,8 +8,7 @@
   :defer t
   :commands (flycheck-mode)
   :init
-  (setq flycheck-mode-line-prefix "F"
-        flycheck-clang-language-standard "c++14")
+  (setq flycheck-mode-line-prefix "F")
 
   (when (memq system-type '(darwin gnu gnu/linux gnu/kfreebsd))
         (add-hook 'c++-mode-hook
@@ -27,8 +16,15 @@
                   (setq flycheck-clang-include-path
                         (list "/usr/local/include")
                         ))))
+
+  ;; language standard for c/c++ files
+  (add-hook 'c++-mode-hook
+            (lambda ()
+              (setq flycheck-clang-language-standard "c++14")))
+
   (dolist (hook '(python-mode-hook
                   clojure-mode-hook
+                  c-mode-hook
                   c++-mode-hook))
     (add-hook hook #'flycheck-mode))
   )
