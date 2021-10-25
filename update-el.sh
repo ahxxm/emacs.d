87e7a2863c73546031eecc6025aa6ababe370b2c
ahxxm <i@ahxxm.com>
2021-10-25 17:03:29 +0800

    sh to update el files

---
+++ update-el.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+wget -O plugins/a.el https://raw.githubusercontent.com/plexus/a.el/master/a.el
+wget -O plugins/f.el https://raw.githubusercontent.com/rejeep/f.el/master/f.el
+wget -O plugins/dockerfile-mode.el https://raw.githubusercontent.com/spotify/dockerfile-mode/master/dockerfile-mode.el
+wget -O plugins/highlight-indentation.el https://raw.githubusercontent.com/antonj/Highlight-Indentation-for-Emacs/master/highlight-indentation.el
+wget -O plugins/highlight-parentheses.el https://git.sr.ht/~tsdh/highlight-parentheses.el/blob/main/highlight-parentheses.el
+wget -O plugins/highlight-symbol.el https://raw.githubusercontent.com/nschum/highlight-symbol.el/master/highlight-symbol.el
+wget -O plugins/markdown-mode.el https://raw.githubusercontent.com/jrblevin/markdown-mode/master/markdown-mode.el
+wget -O plugins/json-mode.el https://raw.githubusercontent.com/joshwnj/json-mode/master/json-mode.el
+wget -O plugins/parseedn.el https://raw.githubusercontent.com/clojure-emacs/parseedn/master/parseedn.el # parseclj module as well
+wget -O plugins/projectile.el https://raw.githubusercontent.com/bbatsov/projectile/master/projectile.el
+wget -O plugins/ring+.el https://www.emacswiki.org/emacs/download/ring%2b.el
+wget -O plugins/s.el https://raw.githubusercontent.com/magnars/s.el/master/s.el
+wget -O plugins/spinner.el https://raw.githubusercontent.com/Malabarba/spinner.el/master/spinner.el
+wget -O plugins/yaml-mode.el https://raw.githubusercontent.com/yoshiki/yaml-mode/master/yaml-mode.el
